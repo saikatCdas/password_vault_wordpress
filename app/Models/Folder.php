63f0971cd741bc1ce1b859b000710ab9bf4d7228
saikat <saikatdas45857@gmail.com>
2023-03-24 14:28:06 +0600

    Frontend folder change

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -29,16 +29,19 @@ class Folder extends Model
 
     public static function changeFolder ($data) {
 
-            // Changing Folder
-            $itemIds = $data['itemsId'];
-            foreach($itemIds as $id){
-                $item = Vault::whereId($id)->first();
-                $item['folder_id'] = $data['folderId'];
-                // return $item['folder_id'];
-                $item->save();
-            }
+        if($data['folderId'] ){
+            $folderId = Folder::whereId($data['folderId'])->exists();
+        }
+        
+        // Changing Folder
+        $itemIds = $data['itemsId'];
+        foreach($itemIds as $id){
+            $item = Vault::whereId($id)->first();
+            $item['folder_id'] = $folderId ? $folderId : $data['folderId'];
+            $item->save();
+        }
 
-            return 'success';
+        return 'success';
     }
 
 
