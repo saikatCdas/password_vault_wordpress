aff2c15e3eeb107b94d8a7a468ca1a01462523ac
saikat <saikatdas45857@gmail.com>
2023-03-30 15:30:23 +0600

    shortcode add vault item

--- assets/js/add-vault-item.js
+++ assets/js/add-vault-item.js
@@ -8,6 +8,7 @@ jQuery(document).ready(function() {
     }
 
 
+    // make post request
     jQuery.ajax({
       url: "http://wordpress.test/wp-json/fluentplugin/v2/get-folder", 
       type: 'GET',
@@ -58,37 +59,37 @@ jQuery(document).ready(function() {
 
 
 const loginComponent = `<div class="mb-4" id="login-username">
-  <label for="user_name" class="block text-md font-semibold text-gray-900 capitalize">User Name</label>
-  <input type="text" id="user_name" name="user_name" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="User name goes here..." />
-</div>
-<div class="mb-4" id="login-email">
-  <label for="email" class="block text-md font-semibold text-gray-900 capitalize">Email</label>
-  <input type="email" id="email" name="email" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="example@something.com"/>
-</div>
+                          <label for="user_name" class="block text-md font-semibold text-gray-900 capitalize">User Name</label>
+                          <input type="text" id="user_name" name="user_name" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="User name goes here..." />
+                        </div>
+                        <div class="mb-4" id="login-email">
+                          <label for="email" class="block text-md font-semibold text-gray-900 capitalize">Email</label>
+                          <input type="email" id="email" name="email" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="example@something.com"/>
+                        </div>
 
-<div class="mb-4" id="login-password" style="display: none;">
-  <label for="password" class="block text-md font-semibold text-gray-900 capitalize">Password</label>
-  <input autocomplete="password" type="password" id="password" name="password" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Password goes here..." />
-</div>
+                        <div class="mb-4" id="login-password" style="display: none;">
+                          <label for="password" class="block text-md font-semibold text-gray-900 capitalize">Password</label>
+                          <input autocomplete="password" type="password" id="password" name="password" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Password goes here..." />
+                        </div>
 
-<div class="mb-4" id="login-url">
-  <label for="url" class="block text-md font-semibold text-gray-900 capitalize">URL</label>
-  <input type="url" id="url" name="url" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="ex. https://google.com"/>
-</div>`;
+                        <div class="mb-4" id="login-url">
+                          <label for="url" class="block text-md font-semibold text-gray-900 capitalize">URL</label>
+                          <input type="url" id="url" name="url" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="ex. https://google.com"/>
+                        </div>`;
 
 const cardComponent = `<div class="mb-4" id="card-card-holder-name">
-<label for="card_holder_name" class="block text-md font-semibold text-gray-900 capitalize">Card Holder Name</label>
-<input v-model="vaultData.card_holder_name" type="text" id="card_holder_name" name="card_holder_name" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Card holder name goes here..." />
-</div>
-<div class="mb-4" id="card-card-number">
-<label for="card_number" class="block text-md font-semibold text-gray-900 capitalize">Card Number</label>
-<input v-model="vaultData.card_number" type="number" id="card_number" name="card_number" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Card Number goes here..."/>
-</div>
-<div class="mb-4" id="card-card-expiration-date">
-<label for="card_expiration_date" class="block text-md font-semibold text-gray-900 capitalize">Expiration Date</label>
-<input v-model="vaultData.card_expiration_date" type="date" id="card_expiration_date" name="card_expiration_date" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Expiration date goes here..." />
-</div>
-<div class="mb-4" id="card-card-securiy-code">
-<label for="card_security_code" class="block text-md font-semibold text-gray-900 capitalize">Security Code</label>
-<input v-model="vaultData.card_security_code" type="text" id="card_security_code" name="card_security_code" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Securite code goes here..." />
-</div>`;
+                          <label for="card_holder_name" class="block text-md font-semibold text-gray-900 capitalize">Card Holder Name</label>
+                          <input v-model="vaultData.card_holder_name" type="text" id="card_holder_name" name="card_holder_name" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Card holder name goes here..." />
+                        </div>
+                        <div class="mb-4" id="card-card-number">
+                          <label for="card_number" class="block text-md font-semibold text-gray-900 capitalize">Card Number</label>
+                          <input v-model="vaultData.card_number" type="number" id="card_number" name="card_number" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Card Number goes here..."/>
+                        </div>
+                        <div class="mb-4" id="card-card-expiration-date">
+                          <label for="card_expiration_date" class="block text-md font-semibold text-gray-900 capitalize">Expiration Date</label>
+                          <input v-model="vaultData.card_expiration_date" type="date" id="card_expiration_date" name="card_expiration_date" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Expiration date goes here..." />
+                        </div>
+                        <div class="mb-4" id="card-card-securiy-code">
+                          <label for="card_security_code" class="block text-md font-semibold text-gray-900 capitalize">Security Code</label>
+                          <input v-model="vaultData.card_security_code" type="text" id="card_security_code" name="card_security_code" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" placeholder="Securite code goes here..." />
+                        </div>`;
